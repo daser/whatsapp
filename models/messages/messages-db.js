807172eeed07f5a021fe9e57369aeda1176e1220
DASER DAVID <daserlinux@gmail.com>
2017-08-16 20:45:47 +0000

    single message should return member object

--- models/messages/messages-db.js
+++ models/messages/messages-db.js
@@ -7,6 +7,7 @@ var MessageSchema = new Schema({
 	    type: Schema.Types.ObjectId,
         ref:'members'
 	},
+	unicorns: [{ type: Schema.ObjectId, ref: 'member' }],
 
 	message: { type: String, required:true, default:"."},
 	
