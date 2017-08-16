cea90ad5e18559a0a26c5f4586ca52f0818ece66
DASER DAVID <daserlinux@gmail.com>
2017-08-16 16:58:21 +0000

    finalize

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ console.log("i got here");
          
         request({uri:'https://whatsappdemo.herokuapp.com/api/message/new',method: "POST", form:{"message":data.message,"messageby":data.messageby}} ,function (error, response, body) {
             if(!error  && response.statusCode == 200){
-                socket.emit("createMsg", body); 
+                socket.emit("createMsg", {message: body}); 
             }else{
                 socket.emit('createMsg', error);
             }
